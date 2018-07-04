c0b615d5433170408dd5e9f77247b8a582ed2ee9
abhi-jha <abhi_jha1995@rediffmail.com>
2018-07-05 01:04:15 +0530

    template specialization

--- Accum.cpp
+++ Accum.cpp
@@ -3,18 +3,28 @@
 //
 #include <iostream>
 #include "Accum.h"
+#include "Person.h"
 void accum(){
     //    Accum<int> integers(90);
-    Accum integers(90);
+    Accum<int> integers(90);
     integers += 3;
     integers += 7;
     std::cout<<integers.getTotal()<<std::endl;
     //Accum<std::string> strings("");
-    Accum strings("");
+    Accum<std::string> strings("");
     strings += "hello";
     strings += " world";
     std::cout<<strings.getTotal()<<std::endl;
 
 //    integers += "dsvbu"; //Type Safety
 //    strings += 2;
+
+    Accum<Person> people(0);
+    Person p1("Kate","Gregory",123);
+    Person p2("Someone","Else",456);
+
+    people += p1;
+    people += p2;
+    std::cout<<people.getTotal()<<std::endl;
+
 }
--- Accum.h
+++ Accum.h
@@ -5,6 +5,8 @@
 #ifndef PATH_CPP_PLURALSIGHT_ACCUM_H
 #define PATH_CPP_PLURALSIGHT_ACCUM_H
 
+#include "Person.h"
+
 template <class T>
 class Accum {
 private:
@@ -15,5 +17,18 @@ public:
     T getTotal() const { return total;}
 };
 
+
+
+//TEMPLATE SPECIALIZATION
+
+template <>
+class Accum<Person> {
+private:
+    int total;
+public:
+    explicit Accum(int start):total(start){}
+    int operator+=(Person const& t){return total = total + t.getNumber();}
+    int getTotal() const { return total;}
+};
 void accum();
 #endif //PATH_CPP_PLURALSIGHT_ACCUM_H
