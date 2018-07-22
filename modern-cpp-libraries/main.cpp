2578da56616e1741058a8e76f964073a2a9f1ea8
abhi-jha <abhi_jha1995@rediffmail.com>
2018-07-23 01:00:56 +0530

    ASSERT satatements; debug vs release builds differences

---
+++ modern-cpp-libraries/main.cpp
@@ -0,0 +1,13 @@
+#include "Precompiled.h"
+
+static_assert(sizeof(double) == 8, "can't serialize floats");
+
+#include <cassert>
+
+auto main() -> int{
+
+    //assert(4 == 5);
+
+    //static_assert(sizeof(double) == 4, "can't serialize floats");
+
+}
